9a0262c2fe38c901bce2fabb94902e15e7c27f45
mimiomia <112013959+mimiomia@users.noreply.github.com>
2025-03-26 23:30:50 +0000

    Extração de título

--- L4E10.py
+++ L4E10.py
@@ -1,10 +1,3 @@
-#Crie um algoritmo que leia dois números
-# multiplique o menor por 10,
-# e divida o maior por 2,
-# some os seus valores e 
-# verifique se o resultado e par,
-# em caso afirmativo exiba a mensagem, o resultado é par, caso contrario, exiba a mensagem, o resultado é impar. 
-
 a = int(input("Digite um número: "))
 b = int(input("Digite um número: "))
 
